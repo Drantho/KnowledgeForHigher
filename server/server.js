1a646d07ec6ae4e7908f4003c3b6fe6c2c71e132
Drantho <drantho@gmail.com>
2021-02-11 17:36:39 -0800

    server.js created, sequelize setup

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const session = require("express-session");
+const compression = require("compression");
+
+const db = require("./models");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(compression());
+
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+
+
+app.use(session({
+    secret: process.env.USER_SECRET,
+
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 2
+    }
+}))
+
+const routes = require("./controllers/routes.js");
+
+
+app.use(routes);
+
+
+const PORT = process.env.PORT || 3000;
+db.sequelize.sync({ force: false }).then(function () {
+    app.listen(PORT, function () {
+        console.log(`App now listening on port: ${PORT} view at: http://localhost:${PORT}`);
+    });
+});
