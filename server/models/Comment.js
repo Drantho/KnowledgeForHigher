46b4116e696b25451a5f4d9cda0a4980452a86a0
Nolan Stucky <nolan@stucky.net>
2021-02-17 15:14:16 -0800

    comment model

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,5 +1,17 @@
-module.exports = function(sequelize, DataTypes) {
+const { Sequelize } = require(".");
+
+module.exports = function (sequelize, DataTypes) {
     var Comment = sequelize.define("Comment", {
-         
-    })
-}
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    });
+
+    Comment.associate = (models) => {
+        Comment.belongsTo(models.User);
+        Comment.belongsTo(models.Question);
+    };
+};
+
+return Comment;
