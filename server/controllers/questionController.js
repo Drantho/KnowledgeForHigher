efe4f718f0250686dafbf2dd8d00832dcac02445
mtrupiano <markt6749@gmail.com>
2021-02-18 19:22:22 -0800

    drafting queries

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -3,17 +3,70 @@ const router = express.Router();
 const db = require('../models');
 
 const { Op } = require('sequelize');
-const { QueryTypes } = require('sequelize');
 
 router.get('/', (request, response) => {
 
+    // If an ID is provided in the query, submit a findOne query and return
     if (request.query.id) {
         db.Question.findOne({
             where: { id: request.query.id }
         }).then( (result) => {
             response.json(result);
+            return;
         }).catch( (err) => {
             response.status(500).json(err);
+            return;
         });
     }
+
+    const includes = [];
+
+    // If a tag name is provided, include the Tags table and specify which tag
+    if (request.query.tag) {
+        includes.push({
+            model: db.Tag,
+            where: { name: request.query.tag },
+            attributes: [],
+            through: { attributes: [] }
+        });
+    }
+    
+    // If a user ID is provided, include the Users table and specify which user
+    if (request.query.user) {
+        includes.push({
+            model: db.User,
+            where: { id: request.query.user },
+            attributes: [],
+            through: { attributes: [] }
+        });
+    }
+
+    // Add a search condition to the query if one is provided
+    const condition = {};
+    if (request.query.search) {
+        condition[[Op.or]] = [
+            { [Op.like]: '%' + request.query.search + '%' },
+            { [Op.like]: '%' + request.query.search + '%' }
+        ];
+    }
+
+    db.Question.findAll({
+        include: includes,
+        where: condition
+    }).then( (result) => {
+        response.json(result);
+    }).catch( (err) => {
+        response.status(500).json(err);
+    });
+
+});
+
+router.get('/unanswered', (request, response) => {
+    db.Question.findAll({
+        where: { isAnswered: false }
+    }).then( (result) => {
+        response.json(result);
+    }).catch( (err) => {
+        response.status(500).json(err);
+    });
 });
