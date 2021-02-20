f93c916c50e3052630e87a10d8a3ca9007a464d3
mtrupiano <markt6749@gmail.com>
2021-02-19 18:44:51 -0800

    initial draft

---
+++ server/controllers/answerController.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+const { Op } = require('sequelize');
+
+router.get('/', (request, response) => {
+    if (request.query.id) {
+        db.Answer.findOne({
+            where: { id: request.query.id }
+        }).then( (result) => {
+            return response.json(result);
+        }).catch( (err) => {
+            return response.status(500).json(err);
+        });
+    }
+
+    // Get all of a question's answers
+    if (request.query.question) {
+        db.Answer.findAll({
+            include: {
+                model: db.Question,
+                where: { id: request.query.question },
+                attributes: []
+            }
+        }).then( (result) => {
+            response.json(result);
+        }).catch( (err) => {
+            response.status(500).json(err);
+        });
+    }
+
+    // Find all of a user's answers
+    if (request.query.user) {
+        db.Answer.findAll({
+            include: {
+                model: db.User,
+                where: { id: request.query.user },
+                attributes: []
+            }
+        }).then( (result) => {
+            response.json(result);
+        }).catch( (err) => {
+            response.status(500).json(err);
+        });
+    }
+});
+
+router.post('/', (request, response) => {
+
+    // Validate user
+
+    // Validate profanity
+
+    db.Answer.create({
+        text: request.body.text,
+        UserId: request.body.user,
+        QuestionId: request.body.question
+    }).then( (result) => {
+        response.json(result);
+    }).catch( (err) => {
+        response.status(500).json(err);
+    });
+});
+
+// Delete an individual Answer (will also delete any of its comments and ratings)
+router.delete('/:id', (request, response) => {
+    db.Answer.destroy({
+        where: { id: request.params.id }
+    }).then( (result) => {
+        response.json(result);
+    }).catch( (err) => {
+        response.status(500).json(err);
+    });
+});
