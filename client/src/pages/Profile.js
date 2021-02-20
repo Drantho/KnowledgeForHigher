1aba58246e85cb1526c23335b0cbc4cc4c810dc7
Drantho <drantho@gmail.com>
2021-02-19 17:10:33 -0800

    users' questions added to profile page.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
+import API from "../utils/API";
 
 export default function Profile() {
+
+    const [questions, setQuestions] = useState([])
+
+    useEffect(()=> {
+        API.getQuestionByUser("1").then(response => {
+            setQuestions(response.data);
+        });
+    }, [])
+
     return (
         <div>
             <h1>
                 Profile Page
             </h1>            
+            <ul>
+                {questions.map(question => <li><strong>{question.title}</strong><p>{question.text}</p></li>)}
+            </ul>
         </div>
     )
 }
