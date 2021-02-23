c04e87fd96e9d46c1663fc874612579219908be1
mtrupiano <markt6749@gmail.com>
2021-02-23 09:40:23 -0800

    added associations to answer and service tables; added type field

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,16 +1,20 @@
-const { Sequelize } = require(".");
 
 module.exports = function (sequelize, DataTypes) {
     var Comment = sequelize.define("Comment", {
         text: {
             type: DataTypes.TEXT,
             allowNull: false
+        },
+        type: {
+            type: DataTypes.ENUM(['question', 'answer', 'service'])
         }
     });
 
     Comment.associate = (models) => {
         Comment.belongsTo(models.User);
         Comment.belongsTo(models.Question);
+        Comment.belongsTo(models.Answer);
+        Comment.belongsTo(models.Service);
     };
     return Comment;
 };
