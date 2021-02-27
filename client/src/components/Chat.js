b554714a40fb92ffa5b2dfd63e5af62a709e2bf8
mtrupiano <markt6749@gmail.com>
2021-02-26 17:27:11 -0800

    commenting

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,6 +1,16 @@
 import { React, useState, useEffect } from 'react';
 
-export default function Chat() {
+
+// Expected props:
+//      id: user's chat ID
+export default function Chat(props) {
+    useEffect( () => {
+        // If user ID does not exist, create one and save it to local storage
+        // If token does not exist, create one
+        //      If token does exist, refresh it and save it to local storage
+        // Generate chat client object
+        // Get list of thread IDs
+    });
     return (
         <div>
             
