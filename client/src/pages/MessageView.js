5253771579a21082e69872251a8d2cf8f36b94f2
mtrupiano <markt6749@gmail.com>
2021-02-28 15:21:24 -0800

    draft of MessageView page

---
+++ client/src/pages/MessageView.js
@@ -0,0 +1,49 @@
+import { React, useState, useEffect }  from 'react';
+import { useParams } from 'react-router-dom';
+import {Box, Text, List} from 'grommet';
+
+import ThreadListItem from '../components/ThreadListItem';
+import ThreadView from '../components/ThreadView';
+
+import messageAPI from '../utils/messageAPI';
+
+export default function MessageView(props) {
+
+    const [threadsList, setThreadsList] = useState([]);
+    const [selectedThread, setSelectedThread] = useState({});
+
+    const handleThreadSelect = (id) => {
+        setSelectedThread({selectedThread: id});
+    }
+
+    useEffect( async () => {
+        console.log(props.userState.id);
+        const loadThreadsList = (await messageAPI.getThreads(props.userState.token)).data;
+        setThreadsList(loadThreadsList);
+        console.log(loadThreadsList);
+    }, []);
+
+    const listData = threadsList.map( (e) => {
+        return {
+            name: e.firstName + ' ' + e.lastName,
+            username: e.userName,
+            threadId: e.id
+        }
+    });
+
+    return (
+        <div>
+            {threadsList.map( (e) => {
+                return <ThreadListItem 
+                            userState={props.userState}
+                            toUser={e.user1 ? e.user1 : e.user2} 
+                            key={e.id} 
+                            threadId={e.id} 
+                            onClick={handleThreadSelect}
+                        />
+            })}
+            
+            <ThreadView userState={props.userState} selectedThread={selectedThread.selectedThread}/>
+        </div>
+    )
+}
