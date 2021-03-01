79276a6d336cd446a80e9ce7a09b67bbc79f4aa3
Drantho <drantho@gmail.com>
2021-03-01 11:01:24 -0800

    show/hide all my/popular tags on userhome added.

--- client/src/pages/UserHome.js
+++ client/src/pages/UserHome.js
@@ -14,8 +14,13 @@ export default function UserHome(props) {
 
     const [questions, setQuestions] = useState([]);
 
+    const [showAllMyTags, setShowAllMyTags] = useState(true);
+
+    const [showAllPopularTags, setShowAllPopularTags] = useState(false);
+
     const fillFeeds = async tagsToFeed => {
         const questionsToFeed = await API.getTagQuestionFeed({ tags: tagsToFeed }, props.userState.token).catch(err => console.log(err));
+        
         setQuestions(questionsToFeed.data);
 
         const servicesToFeed = await API.getTagServiceFeed({ tags: tagsToFeed }, props.userState.token).catch(err => console.log(err));
@@ -34,7 +39,7 @@ export default function UserHome(props) {
         popularTagFeed = popularTagFeed.data;        
 
         popularTagFeed.map(popularTag => {
-            popularTag.show = tagsToFeed.find(tag => tag.name === popularTag.name)            
+            popularTag.show = tagsToFeed.find(tag => tag.name === popularTag.name);
         });
 
         setPopularTags(popularTagFeed);
@@ -66,15 +71,55 @@ export default function UserHome(props) {
         fillFeeds(tags.concat(tempPopular));
     }
 
+    const handleShowMyTags = () => {        
+        setTags(tags.map(tag => {
+            tag.show = !showAllMyTags;
+            return tag;
+        }));
+
+        const tempPopular = popularTags.map(popularTag => {
+            const foundTag = tags.find(tag => tag.name === popularTag.name)
+            if(foundTag){
+                popularTag.show = foundTag.show
+            }
+            return popularTag;          
+        });
+
+        setPopularTags(tempPopular);
+        setShowAllMyTags(!showAllMyTags);
+        fillFeeds(tags.concat(popularTags));
+    }
+
+    const handleShowPopularTags = () => {
+        setPopularTags(popularTags.map(tag => {
+            tag.show = !showAllPopularTags;
+            return tag;
+        }));
+
+        const tempMy = tags.map(myTag => {
+            const foundTag = popularTags.find(tag => tag.name === myTag.name)
+            if(foundTag){
+                myTag.show = foundTag.show
+            }
+            return myTag;          
+        });
+
+        setTags(tempMy);
+        setShowAllPopularTags(!showAllMyTags);
+        fillFeeds(tags.concat(popularTags));
+    }
+
     return (
         <div>
             <h1>User Home</h1>
             <h2>My feed</h2>
             <h3>My Tags</h3>
+            <span onClick={handleShowMyTags}>{showAllMyTags ? "hide" : "show"} all</span>
             <ul>
                 {tags.map(tag => <li key={tag.id}><Link to={`/tag/${tag.id}`}>{tag.name}</Link><img src={tag.show ? `./assets/images/show.png` : `./assets/images/hide.png`} onClick={() => handleHideTag(tag.name)}/></li>)}
             </ul>
             <h3>Popular Tags</h3>
+            <span onClick={handleShowPopularTags}>{showAllPopularTags ? "hide" : "show"} all</span>
             <ul>
                 {popularTags.map(tag => <li key={tag.id}><Link to={`/tag/${tag.id}`}>{tag.name}</Link><img src={tag.show ? `./assets/images/show.png` : `./assets/images/hide.png`} onClick={() => handleHideTag(tag.name)}/></li>)}
             </ul>
