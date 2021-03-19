38f9e1775dba5ebcec4dd88ed241a3a7870c53b9
mtrupiano <markt6749@gmail.com>
2021-03-19 10:55:52 -0700

    set up form to mimic new question form

--- client/src/components/AddServiceForm.js
+++ client/src/components/AddServiceForm.js
@@ -1,35 +1,125 @@
 import { React, useState } from 'react';
 
-import { Box, Form, FormField, Text, TextInput, Button, Heading } from 'grommet';
+import { Grommet, Box, Form, FormField, TextInput, Button, Heading } from 'grommet';
 
 import PostEditor from './PostEditor';
+import TagInput from './TagInput';
+import API from '../utils/API';
 
 export default function AddServiceForm(props) {
 
     const [formValues, setFormValues] = useState({});
+    const [tagNames, setTagNames] = useState([]);
 
     const handleInput = (event) => {
-        setFormValues({ ...formValues, [event.target.name]: event.target.value})
+        setFormValues({ ...formValues, [event.target.name]: event.target.value });
+    }
+
+    const handleSubmit = (event) => {
+        API.createService({
+            ...formValues,
+            tags: tagNames,
+            user: props.userState.id
+        }, props.userState.token).then( (response) => {
+            console.log(response);
+            props.onSubmit();
+        }).catch( (err) => {
+            console.log(err);
+        });
+    }
+
+    const onAddTag = (tag) => {
+        if (tagNames.indexOf(tag) < 0) {
+            setTagNames([...tagNames, tag]);
+        }
+    }
+
+    const theme = {
+        formField: {
+            border: false
+        }, 
+        global: {
+            colors: {
+                focus: {
+                    border: undefined
+                }
+            }
+        },
+        button: {
+            border: { color: '#FCE181' },
+            primary: {
+                color: '#FCE181',
+                border: { color: '#FCE181' }
+            },
+            size: {
+                medium: {
+                    border: {
+                        radius: '8px'
+                    }
+                }
+            },
+            extend: `
+                width: 150px;
+                height: 60px
+            `
+        }
     }
 
     return (
+        <Grommet theme={theme}>
         <Box pad="5px">
-            <Box margin={{ vertical: '15px' }} background='#222E42' round='small'>
+
+            <Box 
+                margin={{ vertical: '15px' }} 
+                background='#222E42' 
+                round='small'
+            >
                 <Heading textAlign='center' alignSelf="center" color='#FCE181' level={3}>
                     Add a service
                 </Heading>
             </Box>
-            <Form>
+
+            <Form onSubmit={handleSubmit}>
                 <FormField label='Service Name' 
                     name='title'
                     htmlFor='title'>
-                    <TextInput name='title' value={formValues.title} onChange={handleInput} />
+                    <TextInput placeholder='Enter a name for your service...'
+                        name='title' 
+                        value={formValues.title} 
+                        onChange={handleInput} 
+                    />
                 </FormField>
-                <PostEditor getDraftValue={ 
-                        val => { setFormValues({ ...formValues, text: JSON.stringify(val) }) }
-                    } controlledInput={formValues.text}
-                    placeholder='Enter a detailed description of your service...' />
+                
+                <FormField label='Description'>
+                    <PostEditor 
+                        getDraftValue={ 
+                            val => { setFormValues({ ...formValues, text: JSON.stringify(val) }) }
+                        } 
+                        controlledInput={formValues.text}
+                        placeholder='Enter a detailed description of your service...' />
+                </FormField>
+
+                <FormField label='Cost' name='price' htmlFor='price'>
+                    <TextInput placeholder='Examples: $5/hour; $100 (one time)'
+                        name='price' 
+                        value={formValues.price}
+                        onChange={handleInput} 
+                    />
+                </FormField>
+
+                <FormField label='Tags' name='tags' htmlFor='new-question-tags'>
+                    <TagInput placeholder='Add tags...'
+                        selectedTags={tagNames} setSelectedTags={setTagNames}
+                        onAddTag={onAddTag} />
+
+                </FormField>
+                
+                <Box align='center'>
+                    <Button primary type='submit' label='Submit' />
+                </Box>
             </Form>
+
         </Box>
+        </Grommet>
     )
 }
