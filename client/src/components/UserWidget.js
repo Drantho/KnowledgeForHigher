3c0ca621a47d19dd026d91691e910a40d7e3f90b
mtrupiano <markt6749@gmail.com>
2021-03-19 11:01:09 -0700

    spacing; corrected portrait URL

--- client/src/components/UserWidget.js
+++ client/src/components/UserWidget.js
@@ -6,17 +6,20 @@ export default function UserWidget(props) {
 
     const history = useHistory();
 
-    const handleClick = (event) => {
-        history.push(`/users/${props.userState.id}`);
-    }
-
     return (
-        <Card elevation='0' background='#FCE181'>
-            <CardBody onClick={handleClick}>
-                <Box pad={{ horizontal: 'small' }} align='center' direction='row'>
+        <Card 
+            style={{ boxShadow: '0 0 3px #9dbacc' }}
+            elevation='0' 
+            background='#FCE181'>
+            <CardBody onClick={() => history.push(`/users/${props.userState.id}`)}>
+                <Box 
+                    pad={{ horizontal: 'small' }} 
+                    align='center' 
+                    direction='row'
+                >
                     <Text>{props.userState.userName}</Text>
-                    <Avatar 
-                        src={props.userState.portrait}/>
+                    <Avatar pad='xsmall' margin='xsmall' size='36px'
+                        src={`https://res.cloudinary.com/drantho/image/upload/c_fill,w_125/${props.userState.portrait}.png`} />
                 </Box>
             </CardBody>
         </Card>
