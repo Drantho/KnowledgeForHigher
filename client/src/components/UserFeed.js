5afa76e337379957c94a2171b155d3f3bf7ee02a
mtrupiano <markt6749@gmail.com>
2021-03-21 14:20:20 -0700

    spacing; removed Date creation statements

--- client/src/components/UserFeed.js
+++ client/src/components/UserFeed.js
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react';
 
-import { Text, Box, Grommet } from 'grommet';
+import { Box, Grommet } from 'grommet';
 
 import API from '../utils/API';
 
@@ -18,28 +18,24 @@ export default function UserFeed(props) {
 
     useEffect( async () => {
 
-        const services = (await API.getServicesByUser(props.targetUser.id)).data;
+        const services  = (await API.getServicesByUser(props.targetUser.id)).data;
         const questions = (await API.getQuestionByUser(props.targetUser.id)).data;
-        const answers = (await API.getAnswersByUser(props.targetUser.id)).data;
-        const comments = (await API.getCommentsByUser(props.targetUser.id)).data;
-        const ratings = (await API.getRatingsByUser(props.targetUser.id)).data;
+        const answers   = (await API.getAnswersByUser (props.targetUser.id)).data;
+        const comments  = (await API.getCommentsByUser(props.targetUser.id)).data;
+        const ratings   = (await API.getRatingsByUser (props.targetUser.id)).data;
 
-        services.forEach(e => (e.entityType = 'service' ))
-        questions.forEach(e => (e.entityType = 'question' ))
-        answers.forEach(e => (e.entityType = 'answer' ))
-        comments.forEach(e => (e.entityType = 'comment' ))
-        ratings.forEach(e => (e.entityType = 'rating' ))
+        services .forEach( e => e.entityType = 'service'  );
+        questions.forEach( e => e.entityType = 'question' );
+        answers  .forEach( e => e.entityType = 'answer'   );
+        comments .forEach( e => e.entityType = 'comment'  );
+        ratings  .forEach( e => e.entityType = 'rating'   );
 
         const entities = [...services, ...questions, ...answers, ...comments, ...ratings];
 
         entities.sort( (a, b) => {
-
-            const aDate = new Date(a.createdAt);
-            const bDate = new Date(b.createdAt);
-
-            if (aDate > bDate) {
+            if (a.createdAt > b.createdAt) {
                 return -1;
-            } else if (aDate < bDate) {
+            } else if (a.createdAt < b.createdAt) {
                 return 1;
             } else {
                 return 0;
