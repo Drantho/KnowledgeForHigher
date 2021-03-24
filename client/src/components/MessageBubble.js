0fd7959aef89662e9d9ec9522a2ef869410f87e0
mtrupiano <markt6749@gmail.com>
2021-03-23 22:59:37 -0700

    spacing; corrected portrait display

--- client/src/components/MessageBubble.js
+++ client/src/components/MessageBubble.js
@@ -12,21 +12,32 @@ export default function MessageBubble(props) {
                 content={<Box><Text size='xsmall'>{props.date}</Text></Box>} 
                 dropProps={{
                     stretch: false,
-                    align:  (props.sentOrRecieved === 'sent' ? {right: 'left'} : {left: 'right'}),
+                    align: (props.sentOrRecieved === 'sent' ? {right: 'left'} : {left: 'right'}),
                     justify: 'end', 
                     width: {max: '150px'},
-                }}>
-            
-            <Box direction='row' align='center'>
-            <Card height='min-content' width={{max: 'medium'}}
-                pad={{vertical: 'xsmall', horizontal:'small'}} 
-                background={props.sentOrRecieved === 'sent' ? 'dark-4' : 'dark-1'} 
-                gap='xsmall'
-                round='medium'>
-                <CardBody><Text size='small'>{props.body}</Text></CardBody>
-            </Card>
-            <Avatar size='medium' src={props.portrait} />
-            </Box>
+                }}
+            >
+                <Box direction='row' gap='small' align='center'>
+                    { props.sentOrRecieved !== 'sent' && 
+                        <Avatar elevation='small'
+                            size='medium' 
+                            src={`https://res.cloudinary.com/drantho/image/upload/c_fill,w_125/` +
+                                `${props.portrait}.png`} 
+                        /> }
+
+                    <Card 
+                        height='min-content' 
+                        width={{max: 'medium'}}
+                        pad={{vertical: '4px', horizontal: '16px'}} 
+                        background={props.sentOrRecieved === 'sent' ? 'dark-4' : 'dark-1'} 
+                        gap='xsmall'
+                        round='medium'
+                    >
+                        <CardBody>
+                            <Text size='12pt'>{props.body}</Text>
+                        </CardBody>
+                    </Card>
+                </Box>
             </Tip>
         </Box>
         </Grommet>
