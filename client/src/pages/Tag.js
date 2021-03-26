0f1de50bb62d9d75c4378d74310146f9b8199e0c
mtrupiano <markt6749@gmail.com>
2021-03-25 20:30:09 -0700

    adjusted layout; added navbar; tag page now accessible with name in URL

--- client/src/pages/Tag.js
+++ client/src/pages/Tag.js
@@ -1,16 +1,15 @@
 import { React, useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import API from '../utils/API';
-import { Box, Grid, Heading, Button } from 'grommet';
-import { Add } from 'grommet-icons';
-import TagQuestion from '../components/TagQuestion'
-import FollowedServices from '../components/FollowedServices'
-import Service from '../components/Service'
+import { Box, Heading, Anchor } from 'grommet';
 import EntityCard from '../components/EntityCard';
+import Navbar from '../components/Navbar';
+import NothingHereDisplay from '../components/NothingHereDisplay';
 
 
 export default function Tag(props) {
     const { id } = useParams();
+    const history = useHistory();
     const [entitiesList, setEntitiesList] = useState([]);
     const [tag, setTag] = useState({ 
         name: "",
@@ -20,30 +19,68 @@ export default function Tag(props) {
     });
 
     useEffect(() => {
-        API.getTagById(id).then(response => {
-            const data = response.data;
-            console.log(data);
-            data.Services.forEach( e => e.type = 'service');
-            data.Questions.forEach( e => e.type = 'question');
-            
-            const entities = response.data.Questions.concat(response.data.Services);
-            setEntitiesList(entities);
-            setTag(response.data);
-        }).catch(err => {
-            console.log(`oops!`, err);
-        });
+        if ( !isNaN(parseInt(id)) && typeof (parseInt(id)) === 'number') {
+            API.getTagById(id).then( (response) => {
+                const data = response.data;
+                data.Services.forEach( e => e.type = 'service');
+                data.Questions.forEach( e => e.type = 'question');
+                
+                const entities = data.Questions.concat(data.Services);
+                setEntitiesList(entities);
+                setTag(data);
+            }).catch(err => {
+                console.log(`oops!`, err);
+            });
+        } else {
+            API.getTagbyName(id).then( (response) => {
+                const data = response.data;
+                data.Services.forEach( e => e.type = 'service');
+                data.Questions.forEach( e => e.type = 'question');
+                
+                const entities = data.Questions.concat(data.Services);
+                setEntitiesList(entities);
+                setTag(data);
+            }).catch(err => {
+                console.log(`oops!`, err);
+            });
+        }
     }, []);
 
     return (
+        <Box width='100%'>
+            <Navbar userState={props.userState} />
 
-        <Box gap='small' fill align='center' width='100%' margin={{top: '100px'}}>
-            <Heading level={2}>
-                Tagged: '{tag.name}'
-            </Heading>
-            {entitiesList.map( (e) => {
-                console.log(e);
-                return <EntityCard userState={props.userState} entity={e} />
-            })}
+            <Box gap='small' fill align='center' width='100%' margin={{top: '100px'}}>
+                { history.length > 0 &&
+                    <Anchor 
+                        margin={{ left: '5%' }} 
+                        alignSelf='start' 
+                        onClick={() => history.goBack()}
+                    >
+                        &lt; Back
+                    </Anchor> }
+
+                <Heading margin={{ vertical: '5px' }} level={2}>
+                    Tagged: '{tag.name}'
+                </Heading>
+  
+                <Box width='90%' height='3px' background='#222e42' />
+
+                { entitiesList.length > 0 ? 
+                    entitiesList.map( e => <EntityCard width='90%'
+                                                showUser 
+                                                userState={props.userState} 
+                                                entity={e} /> )
+                
+                    :
+                    <NothingHereDisplay
+                        container={{
+                            width: '70%',
+                            pad: {
+                                vertical: 'medium'
+                            }
+                        }} /> }
+            </Box>
         </Box>
     )
 }
