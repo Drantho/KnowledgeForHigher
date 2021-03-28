097c4d778baff11c5c9d9bb8eb556b4bdd42e4f8
mtrupiano <markt6749@gmail.com>
2021-03-28 13:28:53 -0700

    tweaking padding; added logic for only showing portrait next to last in chain of messages from same user

--- client/src/components/MessageBubble.js
+++ client/src/components/MessageBubble.js
@@ -23,22 +23,23 @@ export default function MessageBubble(props) {
                     direction='row'  
                     gap='small' 
                     align='center'
-                    margin={ props.sentOrRecieved !== 'sent' && props.showPortrait && { top: '-10px' }}
+                    margin={ (props.sentOrRecieved !== 'sent' && props.showPortrait) ? 
+                        { top: '-10px' } : undefined }
                 >
                     { props.sentOrRecieved !== 'sent' && props.showPortrait && 
                         <Avatar 
                             elevation='small'
                             size='medium' 
                             src={`https://res.cloudinary.com/drantho/image/upload/c_fill,w_125/` +
-                                `${props.portrait}.png`} 
+                                `${props.portrait}.png` } 
                         /> }
                     
                         {props.sentOrRecieved !== 'sent' && !props.showPortrait && <Box width='48px' />}
 
                     <Card 
                         height='min-content' 
-                        width={{max: 'medium'}}
-                        pad={{vertical: '4px', horizontal: '16px'}} 
+                        width={{ max: 'medium' }}
+                        pad={{ vertical: '4px', horizontal: '16px' }} 
                         background={props.sentOrRecieved === 'sent' ? 'dark-4' : 'dark-1'} 
                         gap='xsmall'
                         round='medium'
